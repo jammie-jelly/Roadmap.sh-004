23f7272421b1f9dcfce87dfb44380697877926fb
jammie-jelly <105651142+jammie-jelly@users.noreply.github.com>
2024-11-05 23:25:10 +0300

    revert modification

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb://mongo:27017')
+mongoose.connect('mongodb://mongo:27017/todos')
     .then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.error("MongoDB connection error:", err));
 
